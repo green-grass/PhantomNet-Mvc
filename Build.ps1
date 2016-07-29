c540b5cface0e745d47f7f89b3d3f110bae29e62
mnguyen284 <mnguyen284@gmail.com>
2016-07-29 13:05:32 +0700

    Build

--- Build.ps1
+++ Build.ps1
@@ -62,6 +62,6 @@ $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 # exec { & dotnet test .\test\PhantomNet.Core.Tests -c Release }
 
 exec { & dotnet pack .\src\PhantomNet.AspNetCore.Mvc -c Release -o .\artifacts --version-suffix=$revision }
-exec { & dotnet pack .\src\extensions\PhantomNet.AspNetCore.Mvc.SharedStrings -c Release -o .\artifacts --version-suffix=$revision }
+exec { & dotnet pack .\src\PhantomNet.AspNetCore.Mvc.SharedStrings -c Release -o .\artifacts --version-suffix=$revision }
 exec { & dotnet pack .\src\extensions\PhantomNet.AspNetCore.Mvc.Views -c Release -o .\artifacts --version-suffix=$revision }
 exec { & dotnet pack .\src\aspnet\PhantomNet.AspNetCore.Mvc.DataAnnotations -c Release -o .\artifacts --version-suffix=$revision }
