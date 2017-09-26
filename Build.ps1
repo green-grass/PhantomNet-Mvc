9e6e9ab8a398321f8b4f73e87c0b1ca573567560
Minh Nguyen <mnguyen284@gmail.com>
2017-09-26 23:32:26 +0700

    Build

--- Build.ps1
+++ Build.ps1
@@ -26,9 +26,6 @@ if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
 exec { & dotnet restore }
 
-$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
-
 # exec { & dotnet test .\test\PhantomNet.Core.Tests -c Release }
 
 if ($env:APPVEYOR_REPO_TAG -eq $true) {
